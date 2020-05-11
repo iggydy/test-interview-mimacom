42d8df9bec82013d06b53a9831160790c7afcab4
iggy_dy <ifernandezliebana@gmail.com>
2020-05-11 11:07:38 +0200

    arrows menu transition and body hidden menu active

--- src/components/ArrowCart.vue
+++ src/components/ArrowCart.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="ArrowCart" @click="activeMenuArrow">
-    <div class="arrow"></div>
-    <div class="arrow"></div>
-    <div class="arrow"></div>
+    <div  class="ArrowCart__container" :class="{'active-cart-menu' : menuActive}">
+      <div class="ArrowCart__container__arrow" :class="{'active-arrow-top' : menuActive}"></div>
+      <div class="ArrowCart__container__arrow" :class="{'active-arrow-mid' : menuActive}"></div>
+      <div class="ArrowCart__container__arrow" :class="{'active-arrow-bot' : menuActive}"></div>
+    </div>
   </div>
 </template>
 
@@ -22,6 +24,11 @@ export default {
   methods: {
     activeMenuArrow() {
       store.commit('activeMenu')
+      if ( this.menuActive ) {
+        document.body.style.overflow = "hidden"
+      } else {
+        document.body.style.overflow = "auto"
+      }
     }
   }
 }
@@ -36,16 +43,44 @@ export default {
   right: 25px;
   display: none;
 
+  &__container {
+    position: relative;
 
-  .arrow {
-    width: 25px;
-    height: 3px;
-    background: #000;
-    margin-bottom: 5px;
+    &__arrow {
+      width: 25px;
+      height: 3px;
+      background: #000;
+      margin-bottom: 5px;
+      transition: 0.3s ease all;
 
-    &:last-child {
-      margin: 0;
+      &:last-child {
+        margin: 0;
+      }
     }
+  }
+
+
+  .active-cart-menu {
+    right: 25px;
+    top: 7px;
+  }
+
+  .active-arrow-top {
+    position: absolute;
+    transform: rotate(45deg);
+
+  }
+
+  .active-arrow-mid {
+    display: none;
+    
+  }
+
+  .active-arrow-bot {
+    position: absolute;
+    transform: rotate(-45deg);
+
+    
   }
 
 
